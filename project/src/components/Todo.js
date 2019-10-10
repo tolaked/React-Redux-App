7529d4396651fce7de682fff94721997e1efda1a
Akere Adetola <adetolaakere1@gmail.com>
2019-10-10 18:40:53 +0100

    styled component

--- project/src/components/Todo.js
+++ project/src/components/Todo.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { getTodos } from "../state/actionCreator";
 import { connect } from "react-redux";
+import Styled from "styled-components";
 
 const Todo = props => {
   useEffect(() => {
@@ -9,16 +10,18 @@ const Todo = props => {
   const myTodos = [...props.state.allTodo];
 
   return (
-    <div>
-      <h5>Hello</h5>
-      {myTodos.map((el, id) => (
-        <div key={id}>
-          <h5>{el.name}</h5>
-          <p>{el._id}</p>
-          <p>{el.created_at}</p>
-        </div>
-      ))}
-    </div>
+    <StyledTodo className="allTodo">
+      {myTodos.map(
+        (el, id) =>
+          el._id && (
+            <StyledDiv key={id}>
+              <h5>Name:{el.name}</h5>
+              <p>id:{el._id}</p>
+              <p>Created at:{el.created_at}</p>
+            </StyledDiv>
+          )
+      )}
+    </StyledTodo>
   );
 };
 
@@ -26,3 +29,19 @@ export default connect(
   state => ({ state: state }),
   dispatch => ({ getTodos: () => dispatch(getTodos()) })
 )(Todo);
+
+const StyledTodo = Styled.div`
+text-align:center;
+display:flex;
+flex-wrap:wrap;
+margin:0 auto
+margin-left:10px;
+
+`;
+const StyledDiv = Styled.div`
+border:1px solid black;
+margin-bottom:10px;
+margin-right:10px;
+width:300px;
+background-color:
+`;
